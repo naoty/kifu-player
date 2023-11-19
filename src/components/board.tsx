4ed91ccb1118df8ceb1f9ba33aa121b2e2ae07c7
Naoto Kaneko <naoty.k@gmail.com>
2023-11-19 22:26:20 +0900

    マスをループで生成する

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -59,7 +59,8 @@ const initialPositions = new Map([
 export default function Board() {
   const [positions, setPositions] = useState<Positions>(initialPositions)
 
-  const pieceAt = (position: number) => {
+  const pieceAt = (row: number, column: number) => {
+    const position = row + column * 10;
     const props = positions.get(position)
     if (props) {
       return <Piece type={props.type} owner={props.owner} />
@@ -67,128 +68,53 @@ export default function Board() {
     return null
   }
 
+  const kanjiNumber = (num: number) => {
+    switch (num) {
+      case 1:
+        return '一'
+      case 2:
+        return '二'
+      case 3:
+        return '三'
+      case 4:
+        return '四'
+      case 5:
+        return '五'
+      case 6:
+        return '六'
+      case 7:
+        return '七'
+      case 8:
+        return '八'
+      case 9:
+        return '九'
+      default:
+        return ''
+    }
+  }
+
+  const squares = []
+  for (let row = 0; row <= 9; row++) {
+    const rows = [];
+    for (let column = 1; column <= 9; column++) {
+      if (row === 0) {
+        rows.push(<ColumnNumber value={`${10 - column}`} />)
+      } else {
+        rows.push(<Square>{pieceAt(row, column)}</Square>)
+      }
+    }
+    squares.push(
+      <tr>
+        {...rows}
+        {row > 0 && <RowNumber value={kanjiNumber(row)} />}
+      </tr>
+    )
+  }
+
   return (
     <table className={`${classNames.board}`}>
       <tbody>
-        <tr>
-          <ColumnNumber value='9' />
-          <ColumnNumber value='8' />
-          <ColumnNumber value='7' />
-          <ColumnNumber value='6' />
-          <ColumnNumber value='5' />
-          <ColumnNumber value='4' />
-          <ColumnNumber value='3' />
-          <ColumnNumber value='2' />
-          <ColumnNumber value='1' />
-        </tr>
-        <tr>
-          <Square>{pieceAt(91)}</Square>
-          <Square>{pieceAt(81)}</Square>
-          <Square>{pieceAt(71)}</Square>
-          <Square>{pieceAt(61)}</Square>
-          <Square>{pieceAt(51)}</Square>
-          <Square>{pieceAt(41)}</Square>
-          <Square>{pieceAt(31)}</Square>
-          <Square>{pieceAt(21)}</Square>
-          <Square>{pieceAt(11)}</Square>
-          <RowNumber value='一' />
-        </tr>
-        <tr>
-          <Square>{pieceAt(92)}</Square>
-          <Square>{pieceAt(82)}</Square>
-          <Square>{pieceAt(72)}</Square>
-          <Square>{pieceAt(62)}</Square>
-          <Square>{pieceAt(52)}</Square>
-          <Square>{pieceAt(42)}</Square>
-          <Square>{pieceAt(32)}</Square>
-          <Square>{pieceAt(22)}</Square>
-          <Square>{pieceAt(12)}</Square>
-          <RowNumber value='二' />
-        </tr>
-        <tr>
-          <Square>{pieceAt(93)}</Square>
-          <Square>{pieceAt(83)}</Square>
-          <Square>{pieceAt(73)}</Square>
-          <Square>{pieceAt(63)}</Square>
-          <Square>{pieceAt(53)}</Square>
-          <Square>{pieceAt(43)}</Square>
-          <Square>{pieceAt(33)}</Square>
-          <Square>{pieceAt(23)}</Square>
-          <Square>{pieceAt(13)}</Square>
-          <RowNumber value='三' />
-        </tr>
-        <tr>
-          <Square>{pieceAt(94)}</Square>
-          <Square>{pieceAt(84)}</Square>
-          <Square>{pieceAt(74)}</Square>
-          <Square>{pieceAt(64)}</Square>
-          <Square>{pieceAt(54)}</Square>
-          <Square>{pieceAt(44)}</Square>
-          <Square>{pieceAt(34)}</Square>
-          <Square>{pieceAt(24)}</Square>
-          <Square>{pieceAt(14)}</Square>
-          <RowNumber value='四' />
-        </tr>
-        <tr>
-          <Square>{pieceAt(95)}</Square>
-          <Square>{pieceAt(85)}</Square>
-          <Square>{pieceAt(75)}</Square>
-          <Square>{pieceAt(65)}</Square>
-          <Square>{pieceAt(55)}</Square>
-          <Square>{pieceAt(45)}</Square>
-          <Square>{pieceAt(35)}</Square>
-          <Square>{pieceAt(25)}</Square>
-          <Square>{pieceAt(15)}</Square>
-          <RowNumber value='五' />
-        </tr>
-        <tr>
-          <Square>{pieceAt(96)}</Square>
-          <Square>{pieceAt(86)}</Square>
-          <Square>{pieceAt(76)}</Square>
-          <Square>{pieceAt(66)}</Square>
-          <Square>{pieceAt(56)}</Square>
-          <Square>{pieceAt(46)}</Square>
-          <Square>{pieceAt(36)}</Square>
-          <Square>{pieceAt(26)}</Square>
-          <Square>{pieceAt(16)}</Square>
-          <RowNumber value='六' />
-        </tr>
-        <tr>
-          <Square>{pieceAt(97)}</Square>
-          <Square>{pieceAt(87)}</Square>
-          <Square>{pieceAt(77)}</Square>
-          <Square>{pieceAt(67)}</Square>
-          <Square>{pieceAt(57)}</Square>
-          <Square>{pieceAt(47)}</Square>
-          <Square>{pieceAt(37)}</Square>
-          <Square>{pieceAt(27)}</Square>
-          <Square>{pieceAt(17)}</Square>
-          <RowNumber value='七' />
-        </tr>
-        <tr>
-          <Square>{pieceAt(98)}</Square>
-          <Square>{pieceAt(88)}</Square>
-          <Square>{pieceAt(78)}</Square>
-          <Square>{pieceAt(68)}</Square>
-          <Square>{pieceAt(58)}</Square>
-          <Square>{pieceAt(48)}</Square>
-          <Square>{pieceAt(38)}</Square>
-          <Square>{pieceAt(28)}</Square>
-          <Square>{pieceAt(18)}</Square>
-          <RowNumber value='八' />
-        </tr>
-        <tr>
-          <Square>{pieceAt(99)}</Square>
-          <Square>{pieceAt(89)}</Square>
-          <Square>{pieceAt(79)}</Square>
-          <Square>{pieceAt(69)}</Square>
-          <Square>{pieceAt(59)}</Square>
-          <Square>{pieceAt(49)}</Square>
-          <Square>{pieceAt(39)}</Square>
-          <Square>{pieceAt(29)}</Square>
-          <Square>{pieceAt(19)}</Square>
-          <RowNumber value='九' />
-        </tr>
+        {...squares}
       </tbody>
     </table>
   )
