fa7903e4aa8bfc50f99a9abe40262c518942cbe9
Naoto Kaneko <naoty.k@gmail.com>
2023-11-24 17:12:49 +0900

    KifuPlayerコンポーネントを定義する

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,11 @@
-import Board from "./components/board";
+import KifuPlayer from "./components/kifu-player";
 
 export default function App() {
   return (
     <>
       <h1 className="header">将棋盤コンポーネント実験室</h1>
       <section className="content">
-        <Board />
+        <KifuPlayer />
       </section>
     </>
   )
---
+++ src/components/kifu-player.tsx
@@ -0,0 +1,9 @@
+import Board from "./board";
+
+export default function KifuPlayer() {
+  return (
+    <div>
+      <Board />
+    </div>
+  )
+}
