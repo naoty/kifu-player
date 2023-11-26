9eb5da329d6bf5d394b50d24965c2f9a88885285
Naoto Kaneko <naoty.k@gmail.com>
2023-11-26 21:37:17 +0900

    パッケージに型定義ファイルを含める

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import react from '@vitejs/plugin-react-swc'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({ rollupTypes: true }),
+  ], 
   build: {
     lib: {
       entry: resolve(__dirname, './src/lib/main.ts'),
