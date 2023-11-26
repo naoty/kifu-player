6f5af1e54fb00c2a853b0b5f8350635a8d4f7b9a
Naoto Kaneko <naoty.k@gmail.com>
2023-11-26 21:32:02 +0900

    viteのビルド設定を追加する

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, './src/lib/main.ts'),
+      name: 'KifuPlayer'
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React'
+        }
+      }
+    }
+  }
 })
