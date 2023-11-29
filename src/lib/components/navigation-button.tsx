d2fe8f56eab46d439fe3af1ff9f4f9a214406704
Naoto Kaneko <naoty.k@gmail.com>
2023-11-29 21:43:43 +0900

    ナビゲーションボタンにクラス名を付与する

--- src/lib/components/navigation-button.tsx
+++ src/lib/components/navigation-button.tsx
@@ -9,10 +9,10 @@ type Props = {
 
 export default function NavigationButton({ onBackButtonClick, onNextButtonClick, className }: Props) {
   return (
-    <div className={clsx(className, classNames.container)}>
+    <div className={clsx('navigation', className, classNames.container)}>
       <button
         onClick={onBackButtonClick}
-        className={`${classNames.button} ${classNames.back}`}
+        className={clsx('back-button', classNames.button, classNames.back)}
       >
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 -960 960 960" className={`${classNames.icon}`}>
           <path d="M560-240 320-480l240-240 56 56-184 184 184 184-56 56Z"/>
@@ -20,7 +20,7 @@ export default function NavigationButton({ onBackButtonClick, onNextButtonClick,
       </button>
       <button
         onClick={onNextButtonClick}
-        className={`${classNames.button}`}
+        className={clsx('next-button', classNames.button)}
       >
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 -960 960 960" className={`${classNames.icon}`}>
           <path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z"/>
