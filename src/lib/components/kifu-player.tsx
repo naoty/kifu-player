f7e525e4698c2866300244bffb01352eecf9183d
Naoto Kaneko <naoty.k@gmail.com>
2023-12-03 16:51:43 +0900

    SFENPlayerを追加する
    エラーバウンダリーを設けるため従来のKifuPlayerをwrapする
    親コンポーネントを定義する。

--- src/lib/components/kifu-player.tsx
+++ src/lib/components/kifu-player.tsx
@@ -1,52 +1,10 @@
-import { useState } from "react";
-import clsx from "clsx";
-import classNames from "./kifu-player.module.css";
-import Board from "./board";
-import Hand from "./hand";
-import NavigationButton from "./navigation-button";
-import { parseSFEN } from "../parse-sfen";
-import { Color } from "../piece";
+import SFENPlayer from "./sfen-player"
 
-export default function KifuPlayer({ sfen, className }: { sfen: string, className?: string }) {
-  const positions = parseSFEN(sfen)
-  const [positionNumber, setPositionNumber] = useState(1)
-  const position = positions[positionNumber - 1]
+type Props = {
+  sfen: string,
+  className?: string,
+}
 
-  const onBackButtonClick = () => {
-    if (positionNumber > 1) {
-      setPositionNumber(positionNumber - 1)
-    }
-  }
-
-  const onNextButtonClick = () => {
-    if (positionNumber < positions.length) {
-      setPositionNumber(positionNumber + 1)
-    }
-  }
-
-  return (
-    <div className={clsx('kifu-player', classNames.kifu_player, className)}>
-      <Hand
-        pieces={position.whiteHand}
-        color={Color.White}
-        className={`${classNames.white_hand}`}
-      />
-      <Board
-        pieces={positions[positionNumber - 1].board}
-        destination={position.destination}
-        className={`${classNames.board}`}
-      />
-      <Hand
-        pieces={position.blackHand}
-        color={Color.Black}
-        className={`${classNames.black_hand}`}
-      />
-      <nav className={`${classNames.navigation}`}>
-        <NavigationButton
-          onBackButtonClick={onBackButtonClick}
-          onNextButtonClick={onNextButtonClick}
-        />
-      </nav>
-    </div>
-  )
+export default function KifuPlayer({ sfen, className }: Props) {
+  return <SFENPlayer sfen={sfen} className={className} />
 }
---
+++ src/lib/components/sfen-player.tsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import clsx from "clsx";
+import classNames from "./sfen-player.module.css";
+import Board from "./board";
+import Hand from "./hand";
+import NavigationButton from "./navigation-button";
+import { parseSFEN } from "../parse-sfen";
+import { Color } from "../piece";
+
+export default function SFENPlayer({ sfen, className }: { sfen: string, className?: string }) {
+  const positions = parseSFEN(sfen)
+  const [positionNumber, setPositionNumber] = useState(1)
+  const position = positions[positionNumber - 1]
+
+  const onBackButtonClick = () => {
+    if (positionNumber > 1) {
+      setPositionNumber(positionNumber - 1)
+    }
+  }
+
+  const onNextButtonClick = () => {
+    if (positionNumber < positions.length) {
+      setPositionNumber(positionNumber + 1)
+    }
+  }
+
+  return (
+    <div className={clsx('kifu-player', classNames.kifu_player, className)}>
+      <Hand
+        pieces={position.whiteHand}
+        color={Color.White}
+        className={`${classNames.white_hand}`}
+      />
+      <Board
+        pieces={positions[positionNumber - 1].board}
+        destination={position.destination}
+        className={`${classNames.board}`}
+      />
+      <Hand
+        pieces={position.blackHand}
+        color={Color.Black}
+        className={`${classNames.black_hand}`}
+      />
+      <nav className={`${classNames.navigation}`}>
+        <NavigationButton
+          onBackButtonClick={onBackButtonClick}
+          onNextButtonClick={onNextButtonClick}
+        />
+      </nav>
+    </div>
+  )
+}
